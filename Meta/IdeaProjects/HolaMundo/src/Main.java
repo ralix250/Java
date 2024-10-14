658fa1aba247c522020796f466d70c1b289dd419
ralix250 <ralix250@gmail.com>
2024-10-13 22:42:12 -0600

    Tema 9 Ejemplo de variables

--- Meta/IdeaProjects/HolaMundo/src/Main.java
+++ Meta/IdeaProjects/HolaMundo/src/Main.java
@@ -2,9 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("HOLA MUNDO DESDE NAPSE");
+        String saludar = "Hola Mundo desde Java";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
 
+        int numero = 10;
+        boolean valor = true;
+        int numero2 = 5;
+        if (valor){
+            System.out.println("numero =" + numero);
+            numero2 = 10;
+        }
+        System.out.println("numero2 = " + numero2);
+
+        var numero3= "15";
     }
 }
