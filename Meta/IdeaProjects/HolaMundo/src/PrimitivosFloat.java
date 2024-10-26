837934034672efc773c96aa2bd80b1cfe49575b4
ralix250 <ralix250@gmail.com>
2024-10-26 12:29:04 -0600

    13. Primitivos numericos de punto flotante

---
+++ Meta/IdeaProjects/HolaMundo/src/PrimitivosFloat.java
@@ -0,0 +1,20 @@
+public class PrimitivosFloat {
+
+    public static void main(String[] args) {
+        float realFloat1 = 1.0f;
+        float realFloat2 = 2120.0f;
+        //Mantiza, exponente y valor
+        float realFloat3 = 2.12e3f;  // esto es lo mismo que 2120.0;
+        float realFloat4 = 1.5e4f;  // esto es lo mismo que 15000;
+        //Notación Cientifica
+        float realFloat5 = 1.5e-10f;  // esto es lo mismo que 0.00000000015
+        //valor real
+        float realFloat6 = 0.00000000015f;  // esto es lo mismo que 1.5e-10f
+        System.out.println(realFloat1);
+        System.out.println(realFloat2);
+        System.out.println(realFloat3);
+        System.out.println(realFloat4);
+        System.out.println(realFloat5);
+        System.out.println(realFloat6);
+    }
+}
