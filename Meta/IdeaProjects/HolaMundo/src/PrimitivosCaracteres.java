3d66a4b6b3c545793ea8f8ec4f9f33c3d21d183a
ralix250 <ralix250@gmail.com>
2024-11-09 11:02:06 -0600

    16 Char y Caracteres Especiales

--- Meta/IdeaProjects/HolaMundo/src/PrimitivosCaracteres.java
+++ Meta/IdeaProjects/HolaMundo/src/PrimitivosCaracteres.java
@@ -1,4 +1,4 @@
-public class Caracteres {
+public class PrimitivosCaracteres {
 /*
  * Todos los tipos primitivos tienen clases equivalentes
  */
@@ -21,11 +21,30 @@ public class Caracteres {
         //uso de var
         var variableChar = '@';
         System.out.println("variableChar: " + variableChar);
-
+        
         //Clase equivalente a char
         System.out.println("char corresponde en byte: " + Character.BYTES);
         System.out.println("char corresponde en bytes: " + Character.SIZE);
         System.out.println("char valor minimo: " + Character.MAX_VALUE);
         System.out.println("char valor maximo: " + Character.MIN_VALUE);
+
+        char espacio      = ' ';      // Espacio en blanco
+        char espacio1     = '\u0020'; // Espacio en blanco (usando código Unicode)
+        char retroceso    = '\b';     // Retroceso (elimina el último carácter en algunas consolas)
+        char tabulador    = '\t';     // Tabulador horizontal (avanza varias posiciones)
+        char nuevaLinea   = '\n';     // Nueva línea (salta a la línea siguiente)
+        char retornoCarro = '\r';     // Retorno de carro (vuelve al inicio de la línea actual)
+
+
+        //Clase equivalente a char
+        /*   Si se utiliza un /t directamente en el codigo se toma como un caracter especial
+        System.out.println("char corresponde en \t byte: " + Character.BYTES);
+        */
+        /*  Se utilizan para obtener el separador de línea del sistema
+            Windows utiliza \r\n como separador de líneas.
+            Linux y macOS utilizan \n.
+        */
+        System.out.println("char valor maximo: " + System.getProperty("lineSeparator") + Character.BYTES);
+        System.out.println("char valor maximo: " + System.lineSeparator() + Character.BYTES);  //Mas usado hoy día
     }
 }
